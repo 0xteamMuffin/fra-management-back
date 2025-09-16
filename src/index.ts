262e8e7a25311d887e68b3aeaf926bcd6d23399a
0xPixelNinja <rkrabha1858@gmail.com>
2025-09-16 17:59:31 +0530

    chore: merge fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { authRoutes } from "./routes/auth.routes";
-import { fraRoutes } from "./routes/fra.routes";
-import { segmentationRoutes } from "./routes/ocr.routes";
+import { apiRouter } from "./api";
 
 dotenv.config();
 
@@ -12,11 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/fra", fraRoutes);
-app.use("/api/v1/analysis", segmentationRoutes);
+app.use("/api", apiRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
